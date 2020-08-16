47635d63327e789e71ac6551e617579fcb0fbd10
EdwardWu <56945729+bluehomewu@users.noreply.github.com>
2020-08-17 00:33:29 +0800

    X00QD: Update to Coral's August fingerprint

--- havoc_X00QD.mk
+++ havoc_X00QD.mk
@@ -39,10 +39,10 @@ TARGET_VENDOR := asus
 TARGET_VENDOR_PRODUCT_NAME := X00QD
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200705.002 6506677 release-keys"
+    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200805.001 6578210 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys
 
 # Official
 export HAVOC_BUILD_TYPE=Official
